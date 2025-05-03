32cbceccae0d03a528f8f17c2cdf197f0986cb07
Asia <133139314+Asilove@users.noreply.github.com>
2025-05-02 22:18:36 -0400

    update image

--- src/data/events.ts
+++ src/data/events.ts
@@ -117,8 +117,11 @@ export const events: Event[] = [
     category: "Sports",
     image: "https://images.unsplash.com/photo-1562077772-3bd90403f7f0",
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
              
     
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
     organizer: "Washington Nationals",
@@ -135,9 +138,13 @@ export const events: Event[] = [
     location: "Carroll County Farm Museum, Westminster, MD",
     category: "Food & Drink",
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     image: "https://images.unsplash.com/photo-1506377247377-2a5b3b417ebb",
              
     
+=======
+    image: "https://th.bing.com/th/id/OIP.ZQTf-9tvdpmEKw5KEUl_XgAAAA?rs=1&pid=ImgDetMain",
+>>>>>>> Stashed changes
 =======
     image: "https://th.bing.com/th/id/OIP.ZQTf-9tvdpmEKw5KEUl_XgAAAA?rs=1&pid=ImgDetMain",
 >>>>>>> Stashed changes
@@ -155,7 +162,11 @@ export const events: Event[] = [
     location: "National Mall, Washington DC",
     category: "Arts",
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     image: "https://images.unsplash.com/photo-1562164038-91cfe1d7cbce",
+=======
+    image: "https://folklife-media.si.edu/images/page_thumbs_large/CFP10999.jpg",
+>>>>>>> Stashed changes
 =======
     image: "https://folklife-media.si.edu/images/page_thumbs_large/CFP10999.jpg",
 >>>>>>> Stashed changes
@@ -173,7 +184,11 @@ export const events: Event[] = [
     location: "Tidal Basin, Washington DC",
     category: "Outdoors",
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     image: "https://images.unsplash.com/photo-1610651218119-cd161e98b319",
+=======
+    image: "https://washington-org.s3.amazonaws.com/s3fs-public/styles/article_hero_breakpoints_theme_washington_xs_1x/public/dc-bike-ride-start-line-shot_credit-dcbr.jpg?itok=kIvlmeB7&timestamp=1519668943",
+>>>>>>> Stashed changes
 =======
     image: "https://washington-org.s3.amazonaws.com/s3fs-public/styles/article_hero_breakpoints_theme_washington_xs_1x/public/dc-bike-ride-start-line-shot_credit-dcbr.jpg?itok=kIvlmeB7&timestamp=1519668943",
 >>>>>>> Stashed changes
