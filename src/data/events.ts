390f992f5788bcaf0c551adcfbdd3bfa306b0140
Hugh S <baron.roomers-0b@icloud.com>
2025-05-04 01:14:35 -0400

    updates images

--- src/data/events.ts
+++ src/data/events.ts
@@ -116,23 +116,6 @@ export const events: Event[] = [
     location: "Nationals Park, Washington DC",
     category: "Sports",
     image: "https://images.unsplash.com/photo-1562077772-3bd90403f7f0",
-<<<<<<< HEAD
-     HEAD
-<<<<<<< Updated upstream
-=======
->>>>>>> parent of 29e4071 (Fix event images and update link in RestaurantEvents component)
-<<<<<<< Updated upstream
-             
-    
-=======
->>>>>>> Stashed changes
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
-=======
- acb2838d99ae103202f2284c663137a2e70cdc91
-=======
->>>>>>> parent of 29e4071 (Fix event images and update link in RestaurantEvents component)
     organizer: "Washington Nationals",
     price: "$25 - $300",
     isFeatured: false,
@@ -146,27 +129,7 @@ export const events: Event[] = [
     time: "12:00 PM - 6:00 PM",
     location: "Carroll County Farm Museum, Westminster, MD",
     category: "Food & Drink",
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
->>>>>>> parent of 29e4071 (Fix event images and update link in RestaurantEvents component)
-<<<<<<< Updated upstream
     image: "https://images.unsplash.com/photo-1506377247377-2a5b3b417ebb",
-             
-    
-=======
-    image: "https://th.bing.com/th/id/OIP.ZQTf-9tvdpmEKw5KEUl_XgAAAA?rs=1&pid=ImgDetMain",
->>>>>>> Stashed changes
-<<<<<<< HEAD
-=======
-    image: "https://th.bing.com/th/id/OIP.ZQTf-9tvdpmEKw5KEUl_XgAAAA?rs=1&pid=ImgDetMain",
->>>>>>> Stashed changes
-=======
-    image: "https://th.bing.com/th/id/OIP.ZQTf-9tvdpmEKw5KEUl_XgAAAA?rs=1&pid=ImgDetMain",
->>>>>>> acb2838d99ae103202f2284c663137a2e70cdc91
-=======
->>>>>>> parent of 29e4071 (Fix event images and update link in RestaurantEvents component)
     organizer: "Maryland Wineries Association",
     price: "$35 in advance, $40 at the door",
     isFeatured: true,
@@ -180,25 +143,7 @@ export const events: Event[] = [
     time: "11:00 AM - 9:00 PM",
     location: "National Mall, Washington DC",
     category: "Arts",
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
->>>>>>> parent of 29e4071 (Fix event images and update link in RestaurantEvents component)
-<<<<<<< Updated upstream
     image: "https://images.unsplash.com/photo-1562164038-91cfe1d7cbce",
-=======
-    image: "https://folklife-media.si.edu/images/page_thumbs_large/CFP10999.jpg",
->>>>>>> Stashed changes
-<<<<<<< HEAD
-=======
-    image: "https://folklife-media.si.edu/images/page_thumbs_large/CFP10999.jpg",
->>>>>>> Stashed changes
-=======
-    image: "https://folklife-media.si.edu/images/page_thumbs_large/CFP10999.jpg",
-acb2838d99ae103202f2284c663137a2e70cdc91
-=======
->>>>>>> parent of 29e4071 (Fix event images and update link in RestaurantEvents component)
     organizer: "Smithsonian Institution",
     price: "Free",
     isFeatured: false,
@@ -212,25 +157,7 @@ acb2838d99ae103202f2284c663137a2e70cdc91
     time: "10:00 AM - 7:00 PM",
     location: "Tidal Basin, Washington DC",
     category: "Outdoors",
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
->>>>>>> parent of 29e4071 (Fix event images and update link in RestaurantEvents component)
-<<<<<<< Updated upstream
     image: "https://images.unsplash.com/photo-1610651218119-cd161e98b319",
-=======
-    image: "https://washington-org.s3.amazonaws.com/s3fs-public/styles/article_hero_breakpoints_theme_washington_xs_1x/public/dc-bike-ride-start-line-shot_credit-dcbr.jpg?itok=kIvlmeB7&timestamp=1519668943",
->>>>>>> Stashed changes
-<<<<<<< HEAD
-=======
-    image: "https://washington-org.s3.amazonaws.com/s3fs-public/styles/article_hero_breakpoints_theme_washington_xs_1x/public/dc-bike-ride-start-line-shot_credit-dcbr.jpg?itok=kIvlmeB7&timestamp=1519668943",
->>>>>>> Stashed changes
-=======
-    image: "https://washington-org.s3.amazonaws.com/s3fs-public/styles/article_hero_breakpoints_theme_washington_xs_1x/public/dc-bike-ride-start-line-shot_credit-dcbr.jpg?itok=kIvlmeB7&timestamp=1519668943",
->>>>>>> acb2838d99ae103202f2284c663137a2e70cdc91
-=======
->>>>>>> parent of 29e4071 (Fix event images and update link in RestaurantEvents component)
     organizer: "DC Bike Club",
     price: "Free",
     isFeatured: true,
